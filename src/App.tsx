57b761c71f1c69d3ea9beeabd7b1d65a3e6d4d95
Bruno Gouveia <78083006+brunovcg@users.noreply.github.com>
2022-11-24 20:01:58 -0300

    feat(initial): deploy

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { StyledTest } from "@/components/Styled";
+
+export default function App() {
+  return (
+    <main>
+      <StyledTest>xxxx</StyledTest>
+    </main>
+  );
+}
---
+++ src/components/Styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledTest = styled.div`
+  width: 100vw;
+  height: 30vh;
+  background-color: red;
+`;
